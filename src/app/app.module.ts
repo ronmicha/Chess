643e1ee419695f1550cc543ee8c902107b993d74
Ron Michaeli <ronmicha@post.bgu.ac.il>
2018-09-01 12:43:21 +0300

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+// Modules
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module'
+
+// Components
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
