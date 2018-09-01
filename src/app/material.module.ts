0aa2355d0a1491083822b2d6082e26b5828bd1a3
Ron Michaeli <ronmicha@post.bgu.ac.il>
2018-09-01 15:12:32 +0300

    background and buttons are set. Styling buttons

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import { MatButtonModule } from '@angular/material/button';
+
 
 const MATERIAL_MODULES = [
-  CommonModule,
+  MatButtonModule
 ];
 
 
